4401f2c0b1113f1895d5577d03c9a1aeae593f12
Martin Aamodt-Leeper <martin.aamodtleeper@yahoo.co.uk>
2020-01-07 18:06:17 +0000

    Added colour to triangles and squares

--- game_engine.py
+++ game_engine.py
@@ -40,10 +40,12 @@ def draw_triangle(triangle):
     t.penup()
     t.goto(triangle.x, triangle.y)
     t.pendown()
+    t.color(triangle.c)
+    t.begin_fill()
     for a in range (3):
         t.forward(triangle.size)
         t.left(120)
-    t.forward(triangle.size)
+    t.end_fill()
 
 def draw_circle(circle):
     t.penup()
@@ -54,23 +56,29 @@ def draw_circle(circle):
     t.circle(circle.r)
     t.end_fill()
 
-def draw_square(triangle):
+def draw_square(square):
     t.penup()
     t.goto(square.x, square.y)
     t.pendown()
+    t.color(square.c)
+    t.begin_fill()
     for i in range(4): 
         t.forward(square.size)
         t.left(90)
+    t.end_fill()
 
 circles = [Circle(0,0,50,"blue"), Circle(60,50,20,"green"), Circle(-60,50,20,"red")]
-triangles = [Triangle(0,-100,50,"blue"), Triangle(560,550,20,"green"), Triangle(440,550,20,"red")]
-squares = [Square(-500,500,50,"blue"), Square(-560,550,20,"green"), Square(-440,550,20,"red")]
+triangles = [Triangle(0,-200,50,"blue"), Triangle(0,-190,20,"green"), Triangle(0,-210,20,"red")]
+squares = [Square(-100,0,50,"blue"), Square(-110,0,20,"green"), Square(-90,0,20,"red")]
 
 for circle in circles:
     draw_circle(circle)
 
 for triangle in triangles:
     draw_triangle(triangle)
+
+for square in squares:
+    draw_square(square)
 
 t.update()
 
